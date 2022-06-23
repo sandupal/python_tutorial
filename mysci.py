ad976042e137a5ce7f7fba260373b134bfc927a2
Sandupal <dutt.sandy@gmail.com>
2022-06-23 15:26:55 -0400

    Converting tempout to floats

--- mysci.py
+++ mysci.py
@@ -1,6 +1,20 @@
-# Read the data file
+# Initialize my data variable
+data = {'date':[], 'time':[], 'tempout':[]}
+# Read and parse the data file
 filename = "data/wxobs20170821.txt"
-with  open(filename, 'r') as datafile:
-  data = datafile.read()
+with open(filename, 'r') as datafile:
 
+  # Read the first three lines (hearder)
+  for _ in range(3):
+    datafile.readline()
+
+  # Read and pase the rest of the file
+  for line in datafile:
+    datum = line.split()
+    data['date'].append(datum[0])
+    data['time'].append(datum[1])
+    data['tempout'].append(float(datum[2]))
+
+
+# DEBUG
 
